7d7b1d9880f3fae0ce91fabb0b549d8e33173abe
krisboz <kristijan.bozinovic1@gmail.com>
2022-04-26 18:32:51 +0200

    Added html/css for the calculator, and basic operating functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(12)
+//add
+function add(a, b) {
+    return(a + b)
+}
+//subtract
+function subtract(a, b) {
+    return(a-b)
+}
+//multiply
+function multiply(a, b) {
+    return(a * b)
+}
+//divide
+function divide(a, b) {
+    return(a/b)
+}
+
+function operate(a, b, operator) {
+    var res= operator(a,b)
+    console.log(res)
+}
+
+operate(1, 3, add)
+
+
